a8d55cade0aad6bd13502affcdea33c42028ee9b
pragati-sacumen <pragati.gupta@sacumen.com>
2023-04-28 11:33:46 +0530

    git commit -m "added a new feature"

--- demo.py
+++ demo.py
@@ -1,2 +1,16 @@
 def add(num1,num2):
     return num1+num2
+#Features
+
+def sum(a : int, b: int ) -> int:
+    """
+    Sum of two numbers
+        args:  
+        a(int): a number 
+        b(int): another number
+
+        returns:
+        sum of a and b (int)
+    """
+    return a+b
+print(sum(90,10))
