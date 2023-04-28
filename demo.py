fdb9aabecf9e67941fd96ccbbaadba06dae0ab6a
pragati-sacumen <pragati.gupta@sacumen.com>
2023-04-28 11:33:46 +0530

    added a new feature

---
+++ demo.py
@@ -0,0 +1,14 @@
+#Features
+
+def sum(a : int, b: int ) -> int:
+    """
+    Sum of two numbers
+        args:  
+        a(int): a number 
+        b(int): another number
+
+        returns:
+        sum of a and b (int)
+    """
+    return a+b
+print(sum(90,10))
